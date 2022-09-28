08fb2b55febcf820b57d534d2e5771948bd337e9
BadChromer <97965193+BadChromer@users.noreply.github.com>
2022-09-28 23:32:18 +0300

    Update sql_commands.sql

--- sql_commands.sql
+++ sql_commands.sql
@@ -3,6 +3,7 @@
 SELECT * 
   FROM Table1 
   WHERE RequestID NOT IN (SELECT RequestID FROM Table2 WHERE RequestID IS NOT NULL);
+
 /*Задача №2*/
 
 SELECT CatalogID, (COUNT(CatalogID) * SUM(PositionPrice * PositionQuantity)) AS PurchasePrice 
