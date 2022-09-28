c6278347286eaea99a84a3ebb83911b37a3a113e
BadChromer <97965193+BadChromer@users.noreply.github.com>
2022-09-28 23:31:55 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,15 @@
-/*1. На странице есть элементы с CSS классом userinput. Написать код на JQuery, который прикрепит
-обработчик onclick на все элементы. Функция обработки пустая.*/
+/*Задача №1*/
 
 $('.userinput').on('click', function() {
     
 });
 
-/*2. Найти все элементы на странице, которые вложены в элемент с id Block1 и имеют класс
-userinput.*/
+/*Задача №2*/
 
 var elems = document.getElementById('Block1').getElementsByClassName('userinput');
 console.log(elems);
 
-/*3. Показать элемент с Id PopupDIV плавно всплывающий(fade) по центру окна браузера.*/
+/*Задача №3*/
 
 $(function() {
     $('#PopupDIV').hide().fadeIn(3000);
